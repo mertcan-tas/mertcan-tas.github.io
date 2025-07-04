9b7d2d3f0ff31389159d6aac2ef4f3650be3f427
Mertcan Taş <mertcan.tas@outlook.com>
2025-07-05 01:58:49 +0300

    content updated

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,4 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+export const SITE_TITLE = 'Mertcan Taş';
+export const SITE_TAGLINE = 'Building Better Software Everyday';
+export const SITE_DESCRIPTION = 'Personal website of Mertcan Taş — Vue & Django developer sharing projects, insights, and developer tools.'
 
-export const SITE_TITLE = 'Devolio';
-export const SITE_TAGLINE = 'Minimalist Starter for Developer Blog & Portfolio Website'; // site tagline
-export const SITE_DESCRIPTION = 'Minimalist Starter for Developer Blog & Portfolio Website'; // site meta description for SEO
